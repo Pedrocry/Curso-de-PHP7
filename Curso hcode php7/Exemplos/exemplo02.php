ba3af58a5d94cb1a70eaffef3f64c31a3aaa5aba
Pedro lima <pedrolima634@gmail.com>
2020-05-24 13:33:39 -0300

    Comit dos exemplos do curso da hcode de PHP

---
+++ Curso hcode php7/Exemplos/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+$anoNascimento=1995;
+$nomeCompleto="Pedro lima";
+$nome1="joao       ";
+$sobrenome="Pedro";
+
+
+$Completo=$nome1."".$sobrenome;
+
+echo $Completo;
+?>
---
+++ Curso hcode php7/Exemplos/exemplo03.php
@@ -0,0 +1,27 @@
+<?php
+//Exemplo de variaveis simples
+
+$nome="Hcode";
+$site="www.hcode.com.br";
+
+$ano=1995;
+$salario=4000.00;
+$bloqueado=false;
+//////////////////////////////
+
+$frutas = array("Abacaxi","laranja","Manga");
+
+//echo $frutas[2];
+
+$nascimento= new DateTime();
+//var_dump($nascimento);
+
+
+$arquivo=fopen("exemplo03.php", "r");
+//var_dump($arquivo);
+
+$nulo=null;
+
+
+
+?>
